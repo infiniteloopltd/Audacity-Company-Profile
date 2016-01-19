d5025b82d8ff97ed31ade9516cdb0dbb62c88b91
Zahidur Rahman Faisal <zr.faisal@programmer.net>
2016-01-19 15:44:28 +0600

    HomeViewController implemented

--- Audacity Company Profile/HomeViewController.swift
+++ Audacity Company Profile/HomeViewController.swift
@@ -8,21 +8,116 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController , UIPageViewControllerDataSource{
 
+    private let contentImages = ["img_home_1.jpg",
+        "img_home_2.jpg",
+        "img_home_3.jpg","img_home_4.jpg","img_home_5.jpg"]
+    
+    private let projectName = ["Amar Phonebook",
+        "Colours FM",
+        "Monasa Learning Centre","Lock Deal","How I Work"]
+    
+    private let projectType = [["Android","iOS"],["Android"],["Website"],["Website","Android"],["Website","Android","iOS"]]
+    
+    var appDelegate:AppDelegate!
+    
+    private var pageViewController: UIPageViewController?
+    @IBOutlet var container: UIView!
+    
+    
+    @IBOutlet var actionBar: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createPageViewController()
+        setupPageControl()
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
+    
+    
+    private func createPageViewController() {
+        
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("ShowcaseController") as! UIPageViewController
+        pageController.dataSource = self
+        
+        if contentImages.count > 0 {
+            let firstController = getItemController(0)!
+            let startingViewControllers: NSArray = [firstController]
+            pageController.setViewControllers((startingViewControllers as [AnyObject] as NSArray as! [UIViewController]), direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        // self.view.addSubview(pageViewController!.view)
+        //pageViewController!.view.frame.size.width = container.frame.size.width
+        
+        pageViewController!.view.frame.size.height = pageViewController!.view.frame.size.height - actionBar.frame.size.height
+        //pageViewController!.view.frame.size.height = container.frame.size.height
+      //  container.addSubview(pageViewController!.view)
+        pageViewController!.view.frame.origin.y = pageViewController!.view.frame.origin.y + actionBar.frame.size.height
+        
+         self.view.addSubview(pageViewController!.view)
+        container.hidden = true;
+        pageViewController!.didMoveToParentViewController(self)
+    }
+    
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.grayColor()
+        appearance.currentPageIndicatorTintColor = UIColor.whiteColor()
+        appearance.backgroundColor = UIColor.darkGrayColor()
+    }
+    
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! ShowcaseViewController
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! ShowcaseViewController
+        
+        if itemController.itemIndex+1 < contentImages.count {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+    
+    private func getItemController(itemIndex: Int) -> ShowcaseViewController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ShowcaseViewController") as! ShowcaseViewController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.imageName = contentImages[itemIndex]
+            pageItemController.nameOfProject = projectName[itemIndex]
+            pageItemController.projectType = projectType[itemIndex]
+            //pageItemController.setButtonText()
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
     override func viewWillAppear(animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
         super.viewDidAppear(animated)
     }
+    
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
     }
--- Audacity Company Profile/ShowcaseViewController.swift
+++ Audacity Company Profile/ShowcaseViewController.swift
@@ -10,26 +10,191 @@ import UIKit
 
 class ShowcaseViewController: UIViewController {
 
+    
+    @IBOutlet var firstCategory: MyCustomButton!
+    
+    @IBOutlet var secondCategory: MyCustomButton!
+    
+    @IBOutlet var thirdCategory: MyCustomButton!
+    
+    
+    @IBOutlet var fourthCategory: MyCustomButton!
+    
+    @IBOutlet var contentImageView: UIImageView!
+    
+    @IBOutlet var projectName: UILabel!
+    
+    
+    
+    var projectType: [String]! ;
+    
+    var itemIndex: Int = 0 // ***
+    // var imageName: String = ""  // ***
+    
+    var imageName: String = "" {
+        
+        didSet {
+            
+            if let imageView = contentImageView {
+                imageView.image = UIImage(named: imageName)
+            }
+            
+        }
+    }
+    
+    
+    var nameOfProject: String = "" {
+        
+        didSet {
+            
+            if let label = projectName {
+                label.text = nameOfProject
+            }
+            
+        }
+    }
+    
+    
+    @IBAction func firstCategoryAction(sender: AnyObject) {
+        
+        if( !firstCategory.hidden ) {
+            
+            /* let alertController = UIAlertController(title: "Audacity IT Solution", message:
+            "You will be redirect tomorrow", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)*/
+            
+           // print("firstbtn clicked if")
+            
+            let url = NSURL(string: "https://google.com")!
+            UIApplication.sharedApplication().openURL(url)
+            
+        } else {
+            print("firstbtn clicked else")
+        }
+        
+    }
+    
+    
+    @IBAction func secondCategoryAction(sender: AnyObject) {
+        
+        if( !secondCategory.hidden ) {
+            /*  let alertController = UIAlertController(title: "Audacity IT Solution", message:
+            "You will be redirect tomorrow", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            */
+            let url = NSURL(string: "https://google.com")!
+            UIApplication.sharedApplication().openURL(url)
+            
+        }
+        
+    }
+    
+    
+    @IBAction func thirdCategoryAction(sender: AnyObject) {
+        
+        if( !thirdCategory.hidden ) {
+            /*          let alertController = UIAlertController(title: "Audacity IT Solution", message:
+            "You will be redirect tomorrow", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            */
+            
+            let url = NSURL(string: "https://google.com")!
+            UIApplication.sharedApplication().openURL(url)
+            
+        }
+        
+    }
+    
+    
+    
+    @IBAction func fourthCategoryAction(sender: AnyObject) {
+        
+        if( !fourthCategory.hidden ) {
+            /*          let alertController = UIAlertController(title: "Audacity IT Solution", message:
+            "You will be redirect tomorrow", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            */
+            
+            let url = NSURL(string: "https://google.com")!
+            UIApplication.sharedApplication().openURL(url)
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        contentImageView!.image = UIImage(named: imageName)
+        projectName!.text = nameOfProject
+        
+        firstCategory.hidden = true
+        secondCategory.hidden =  true
+        thirdCategory.hidden = true
+        fourthCategory.hidden = true
+        
+        setButtonText();
+        
+        
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
+    
+    func setButtonText () {
+        let itemCount = projectType.count
+        var i:Int = 0
+        for ( i = 0; i < itemCount; i++ ) {
+            
+            switch (i) {
+            case 0:
+                firstCategory.setTitle(projectType[i], forState: .Normal)
+                firstCategory.hidden = false
+                break
+                
+            case 1:
+                secondCategory.setTitle(projectType[i], forState: .Normal)
+                secondCategory.hidden = false
+                break
+                
+            case 2:
+                thirdCategory.setTitle(projectType[i], forState: .Normal)
+                thirdCategory.hidden = false
+                break
+            case 3:
+                fourthCategory.setTitle(projectType[i], forState: .Normal)
+                fourthCategory.hidden = false
+                break
+            default:
+                break;
+            }
+            
+        }
+    }
 
 }
