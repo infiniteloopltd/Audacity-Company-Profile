8e2dc4dde2ab707937fc42f587065af46b847584
Zahidur Rahman Faisal <zr.faisal@programmer.net>
2016-01-28 18:41:27 +0600

    SocialViewController implemented

--- Audacity Company Profile/SocialViewController.swift
+++ Audacity Company Profile/SocialViewController.swift
@@ -10,28 +10,44 @@ import UIKit
 
 class SocialViewController: UIViewController {
 
+    @IBAction func facebookBtn(sender: AnyObject) {
+        
+        let url = NSURL(string: "https://www.facebook.com")!
+        UIApplication.sharedApplication().openURL(url)
+        
+    }
+    
+    
+    @IBAction func linkedInBtn(sender: AnyObject) {
+        
+        let url = NSURL(string: "https://www.linkedin.com")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    
+    @IBAction func twitterBtn(sender: AnyObject) {
+        let url = NSURL(string: "https://www.twitter.com")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    
+    @IBAction func youtubeBtn(sender: AnyObject) {
+        let url = NSURL(string: "https://www.youtube.com")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
     override func viewWillAppear(animated: Bool) {
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
         super.viewDidAppear(animated)
@@ -40,5 +56,6 @@ class SocialViewController: UIViewController {
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
     }
+
     
 }
